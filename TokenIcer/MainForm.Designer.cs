a42ba8802789aa2f0568c37d699de2cd9f441d3f
Alan Bryan <icemanind9@gmail.com>
2018-09-11 12:07:26 -0400

    Initial Commit

---
+++ TokenIcer/MainForm.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace TokenIcer
+{
+   partial class MainForm
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.SuspendLayout();
+         // 
+         // MainForm
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+         this.ClientSize = new System.Drawing.Size(1016, 697);
+         this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+         this.MaximizeBox = false;
+         this.Name = "MainForm";
+         this.Text = "TokenIcer";
+         this.ResumeLayout(false);
+
+      }
+
+      #endregion
+   }
+}
+
